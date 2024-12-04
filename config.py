87bf5e2cea9172b8cf329df8201a0d5f7650e992
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-12-04 21:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("7930787536:AAGYFKSprwvHZfdH4KtURNCxCHc5qhTuGT4")
+MONGO_URL = getenv("mongodb+srv://Mohmaya:mohmaya890@cluster0.mesiasx.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
 SUPPORT_GRP = "TG_FRIENDSS"
 UPDATE_CHNL = "VIP_CREATORS"
